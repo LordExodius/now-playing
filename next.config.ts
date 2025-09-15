3ec9693ec5cf554eae77f8a3855d721663d8016a
LordExodius <oscar45697@gmail.com>
2025-09-15 05:48:04 -0400

    add cross origin resource policy for COEP enabled sites

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Resource-Policy",
+            value: "cross-origin",
+          },
+        ],
+      }
+    ]
+  },
 };
 
 export default nextConfig;
